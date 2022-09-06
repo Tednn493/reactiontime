0b09d98785702327237fb88a8ce1b1ad48c0d2b1
Tednn493 <Tedstarv2@hotmail.com>
2022-09-06 13:14:22 +0100

    stage 2 completed
    Ensured all the JS was working as intended

--- script.js
+++ script.js
@@ -1,55 +1,58 @@
-const playButton=document.querySelector("#play")
-const stage1=document.querySelector("#stage1")
-const stage2=document.querySelector("#stage2")
-const stage3=document.querySelector("#stage3")
+const playButton=document.querySelector("#thebutton")
+const textbox=document.querySelector("#textbox")
 const correct=document.querySelector("#correct")
 const incorrect=document.querySelector("#incorrect")
 const reactTime=document.querySelector("#reactTime")
 const gameWindow=document.querySelector(".gameWindow")
 const again=document.querySelector("#again")
+let ctime= new Date()
+let ntime=new Date()
+let rtime= new Date()
+let xnum=0
+let clicked=false
+let tdiff=0
+const rndInt = randomIntFromInterval(5000, 10000)
+console.log(rndInt)
 
-function difference(min){
-    const newtime= new Date()
-    return newtime-min
+function hello(){
+    if (clicked==true){
+        return
+    }else{
+        textbox.innerHTML="CLICK!"
+        gameWindow.style.backgroundColor="green";
+        let c2time= new Date()
+        gameWindow.addEventListener("click", e=> {
+            let n2time= new Date()
+            let diff2= n2time-c2time
+            let reacttext=("Your reaction time is :"+diff2+" ms")
+            textbox.innerHTML=reacttext
+            gameWindow.style.backgroundColor="#242424"
+            return
+        },{ once: true });
+    }
 }
 
-
+function startgame(){
+    ctime= new Date()
+    textbox.innerHTML="Wait for green..."
+    gameWindow.addEventListener("click", e=> {
+        ntime= new Date()
+        let diff= ntime-ctime
+        if (rndInt>diff){
+            clicked=true
+            textbox.innerHTML="You pressed too early"
+            gameWindow.style.backgroundColor="red"
+        }
+    })
+    gameWindow.removeEventListener
+    setTimeout(hello,rndInt)
+}
 
 function randomIntFromInterval(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-function setEventListener(){
-    gameWindow.addEventListener("click", e=>{
-        incorrect.classList.toggle("hidden")
-        gameWindow.style.cssText = "background-color: red"
-        return
-    })
-}
-
-function gotoStage2(){
-    stage1.classList.toggle("hidden")
-    stage2.classList.toggle("hidden")
-    setTimeout(setEventListener(),10000)
-    console.log("Hello")
-}
-
-
-
-
-playButton.addEventListener("click", e=> gotoStage2())
-
-
-function startgame(){
-    stage2.classList.toggle("hidden")
-    stage3.classList.toggle("hidden")
-    gameWindow.style.cssText = "background-color: #00FF00"
-    const currentTime= new Date()   
-}
- 
-  
-const rndInt = randomIntFromInterval(8000, 15000)
-
-
-
-console.log(currentTime.getMilliseconds())
+playButton.addEventListener("click", e=>{
+    startgame()
+    playButton.classList.add("hidden")
+})
